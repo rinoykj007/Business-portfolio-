e5827a24a117ed18aafe117ec631bfa50aa27c80
Rinoy joseph <rinoykj007@example.com>
2024-11-23 14:45:10 +0300

    first commit

---
+++ src/page/index.jsx
@@ -0,0 +1,457 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='flex-row-cd'>
+        <div className='rectangle'>
+          <span className='get-started'>Get Started</span>
+        </div>
+        <div className='prosperix' />
+        <span className='home'>Home</span>
+        <span className='service'>Service</span>
+        <span className='contact'>Contact</span>
+        <span className='help'>Help</span>
+        <span className='sign-in'>Sign in</span>
+      </div>
+      <div className='flex-row-fc'>
+        <div className='rectangle-1'>
+          <div className='flex-row-ae'>
+            <span className='investments'>Investments</span>
+            <div className='rectangle-2' />
+            <div className='rectangle-3' />
+            <div className='rectangle-4' />
+            <div className='rectangle-5' />
+          </div>
+          <div className='flex-row-f'>
+            <span className='span-100'>100</span>
+            <span className='span-200'>200</span>
+            <span className='span-300'>300</span>
+            <span className='span-400'>400</span>
+          </div>
+        </div>
+        <div className='ellipse' />
+        <div className='heading'>
+          <span className='navigating-global-financial-landscape'>
+            Navigating the
+          </span>
+          <span className='global'>Global</span>
+          <span className='navigating-global-financial-landscape-6'>
+            
+            financial landscape
+          </span>
+        </div>
+        <div className='ellipse-7' />
+        <div className='group' />
+        <div className='ellipse-8' />
+        <div className='group-9' />
+        <div className='rectangle-a'>
+          <div className='flex-row-bfb'>
+            <div className='rectangle-b'>
+              <span className='text-f'>48.09</span>
+            </div>
+            <div className='vector'>
+              <div className='vector-c'>
+                <div className='flex-row'>
+                  <div className='ellipse-d' />
+                  <div className='ellipse-e' />
+                </div>
+                <div className='rectangle-f'>
+                  <span className='text-10'>24.08</span>
+                </div>
+                <div className='flex-row-f-10'>
+                  <div className='ellipse-11' />
+                  <div className='ellipse-12' />
+                </div>
+              </div>
+              <div className='line' />
+              <div className='line-13' />
+            </div>
+          </div>
+          <div className='flex-row-da'>
+            <span className='span-10'>10</span>
+            <span className='span-20'>20</span>
+            <span className='span-30'>30</span>
+            <span className='span-40'>40</span>
+            <span className='span-50'>50</span>
+            <span className='span-60'>60</span>
+            <span className='span-70'>70</span>
+          </div>
+        </div>
+        <div className='sub-heading'>
+          <span className='empowering'>Empowering </span>
+          <span className='your-journey-global-financial-success'>
+            Your Journey to Global Financial Success Through Customized and
+            Personalized Financial
+          </span>
+          <span className='c'>C</span>
+          <span className='your-journey-global-financial-success-14'>
+            onsulting for Every Client
+          </span>
+        </div>
+        <div className='ellipse-15' />
+        <span className='dollar'>$</span>
+        <div className='rectangle-16'>
+          <span className='email-address'>Your email address</span>
+          <div className='envelope'>
+            <div className='vector-17' />
+          </div>
+        </div>
+        <div className='rectangle-18'>
+          <span className='get-started-19'>Get Started</span>
+        </div>
+      </div>
+      <div className='mask-group' />
+      <div className='flex-row-f-1a'>
+        <div className='rectangle-1b'>
+          <span className='our-expertise'>OUR EXPERTISE</span>
+        </div>
+        <div className='rectangle-1c'>
+          <div className='rectangle-1d'>
+            <div className='rectangle-1e'>
+              <div className='rectangle-1f' />
+              <div className='rectangle-20' />
+              <div className='rectangle-21' />
+              <div className='rectangle-22' />
+            </div>
+            <div className='rectangle-23' />
+            <div className='rectangle-24' />
+            <div className='rectangle-25'>
+              <span className='percentage'>80%</span>
+            </div>
+            <div className='rectangle-26' />
+            <div className='rectangle-27' />
+            <div className='rectangle-28' />
+            <div className='rectangle-29' />
+            <div className='rectangle-2a' />
+            <div className='rectangle-2b' />
+            <div className='rectangle-2c' />
+          </div>
+          <div className='mask-group-2d' />
+          <div className='rectangle-2e'>
+            <div className='rectangle-2f' />
+            <span className='investment-growth'>Investment growth</span>
+            <div className='rectangle-30'>
+              <span className='income'>Income</span>
+              <div className='group-31' />
+              <div className='flex-row-32'>
+                <div className='rectangle-33' />
+                <span className='utilities'>Utilities</span>
+                <span className='percentage-34'>67%</span>
+                <div className='group-35' />
+              </div>
+              <div className='flex-row-b'>
+                <span className='revenue'>Revenue</span>
+                <span className='percentage-36'>40%</span>
+              </div>
+              <div className='group-37' />
+            </div>
+          </div>
+        </div>
+        <div className='line-38' />
+        <span className='transforming-wealth-management'>
+          Transforming Finances With Strategic Wealth Management
+        </span>
+        <div className='guiding-financial-journey'>
+          <span className='guiding-financial-journey-39'>G</span>
+          <span className='guiding-financial-journey-3a'>
+            uiding You on Your Global Financial Success Journey Through
+            Customized & Personalized Financial Consulting Services.
+          </span>
+          <span className='guiding-financial-journey-3b'>A</span>
+          <span className='achieving-financial-goals'>
+            chieve Your Goals with Data-Driven Expertise and Secure Your
+            Financial Future, Successfully Navigating the Ever-Changing Finance
+          </span>
+        </div>
+        <div className='expert-guidance-strategies'>
+          <span className='we-are'>We're </span>
+          <span className='expert-guidance-strategies-3c'>
+            Committed to Providing You with Expert Guidance, Proven Strategies &
+            Personalized Approach to Ensure Financial Prosperity
+          </span>
+        </div>
+        <div className='rectangle-3d'>
+          <span className='explore-services'>Explore Services</span>
+        </div>
+      </div>
+      <div className='flex-row-aaf'>
+        <div className='rectangle-3e'>
+          <span className='our-services'>OUr services</span>
+        </div>
+        <div className='rectangle-3f'>
+          <div className='rectangle-40'>
+            <div className='rectangle-41'>
+              <div className='rectangle-42' />
+              <div className='rectangle-43' />
+              <div className='rectangle-44' />
+            </div>
+            <div className='rectangle-45' />
+            <div className='rectangle-46' />
+            <div className='rectangle-47' />
+            <div className='rectangle-48' />
+            <div className='rectangle-49' />
+            <div className='rectangle-4a' />
+            <div className='rectangle-4b' />
+            <div className='rectangle-4c' />
+            <div className='rectangle-4d' />
+            <div className='rectangle-4e' />
+            <div className='rectangle-4f' />
+            <div className='rectangle-50' />
+          </div>
+          <div className='mask-group-51' />
+          <div className='rectangle-52'>
+            <span className='investment-growth-53'>Investment growth</span>
+            <div className='rectangle-54'>
+              <span className='percentage-55'>88.7%</span>
+              <div className='flex-row-d'>
+                <span className='percentage-56'>0%</span>
+                <span className='based-on-interest-rate'>
+                  Based on interest rate
+                </span>
+                <span className='percentage-57'>100%</span>
+              </div>
+              <div className='flex-row-a'>
+                <div className='rectangle-58' />
+                <div className='rectangle-59' />
+                <div className='rectangle-5a' />
+              </div>
+            </div>
+          </div>
+          <div className='ellipse-5b' />
+          <div className='ellipse-5c' />
+        </div>
+        <div className='line-5d' />
+        <span className='our-services-can-help'>
+          Our Services Can Help You Grow Your Investments and Wealth
+        </span>
+        <div className='embark-on-a-journey'>
+          <span className='embark'>Embark </span>
+          <span className='on-a-journey-to-unlock'>
+            on a Journey to Unlock a World of Financial Opportunities, Grow Your
+            Investment with Our Comprehensive and Expert Services
+          </span>
+        </div>
+        <div className='rectangle-5e'>
+          <div className='fi-rr-dollar'>
+            <div className='vector-5f' />
+          </div>
+        </div>
+        <div className='rectangle-60'>
+          <div className='fi-rr-chart-pie-alt' />
+        </div>
+        <span className='financial-planning'>Financial Planning</span>
+        <span className='investment-advisory'>Investment Advisory</span>
+        <div className='rectangle-61'>
+          <div className='fi-rr-home'>
+            <div className='vector-62' />
+          </div>
+        </div>
+        <div className='rectangle-63'>
+          <div className='fi-rr-bank'>
+            <div className='vector-64' />
+          </div>
+        </div>
+        <span className='estate-planning'>Estate Planning</span>
+        <span className='wealth-management'>Wealth Management</span>
+        <div className='rectangle-65'>
+          <span className='explore-services-66'>Explore Services</span>
+        </div>
+      </div>
+      <div className='flex-row-c'>
+        <div className='rectangle-67'>
+          <span className='testimonials'>testimonials</span>
+        </div>
+        <div className='line-68' />
+      </div>
+      <span className='what-our-customers-say'>What Our Customers Say</span>
+      <span className='discover-success-stories'>
+        Discover the Success Stories and Satisfaction of Clients Who Have
+        Benefited from Our Expertise and Personalized Financial Guidance
+      </span>
+      <div className='flex-row-69'>
+        <div className='rectangle-6a'>
+          <span className='financial-impact-nonprofit'>
+            Financial expertise has made a significant impact on our nonprofit
+            financial stability, allowing us to better serve our community
+          </span>
+          <span className='quote'>“</span>
+          <div className='mask-group-6b' />
+          <span className='grace-turner'>Grace Turner</span>
+          <span className='director'>Director</span>
+        </div>
+        <div className='rectangle-6c'>
+          <span className='financial-planning-transformation'>
+            Financial planning and investment advice I received from this team
+            completely transformed my future. I couldn't be happier with the
+            results.
+          </span>
+          <span className='quote-6d'>“</span>
+          <div className='mask-group-6e' />
+          <span className='linda-carter'>Linda Carter</span>
+          <span className='analyst'>Analyst</span>
+        </div>
+        <div className='rectangle-6f'>
+          <span className='seamless-estate-planning'>
+            Estate planning is crucial, and they made the process seamless and
+            stress-free. I can rest assured knowing family's future is secure
+          </span>
+          <span className='quote-70'>“</span>
+          <div className='mask-group-71' />
+          <span className='alex-walker'>Alex Walker</span>
+          <span className='attorney'>Attorney</span>
+        </div>
+      </div>
+      <span className='valuable-services'>
+        Our Team Provides Valuable Services at Any Time in Any Situation
+      </span>
+      <div className='flex-row-f-72'>
+        <div className='ellipse-73' />
+        <div className='ellipse-74' />
+        <div className='ellipse-75' />
+        <div className='graduation-cap'>
+          <div className='vector-76' />
+        </div>
+        <div className='shield-check' />
+        <div className='apps-add'>
+          <div className='flex-row-c-77'>
+            <div className='vector-78' />
+            <div className='vector-79' />
+          </div>
+          <div className='flex-row-eb'>
+            <div className='vector-7a' />
+            <div className='vector-7b' />
+          </div>
+        </div>
+      </div>
+      <div className='flex-row-de'>
+        <span className='customized-solutions'>Customized Solutions</span>
+        <div className='regroup'>
+          <span className='expert-guidance'>Expert Guidance</span>
+          <span className='risk-management'>Risk Management</span>
+        </div>
+      </div>
+      <div className='flex-row-b-7c'>
+        <span className='tailored-financial-advice'>
+          Rely on seasoned professionals for tailored financial advice and
+          strategic planning aligned with your unique goals
+        </span>
+        <span className='financial-risk-mitigation'>
+          We expertly handle and proactively mitigate financial risks, ensuring
+          the safeguard of assets and overall stability
+        </span>
+        <span className='financial-solutions'>
+          Receive and Benefit from with financial solutions that tailored to
+          your unique financial challenges and aspirations
+        </span>
+      </div>
+      <div className='flex-row-ef'>
+        <span className='read-more'>Read more</span>
+        <span className='read-more-7d'>Read more</span>
+        <span className='read-more-7e'>Read more</span>
+      </div>
+      <div className='rectangle-7f'>
+        <div className='image' />
+        <span className='start-now'>Start now</span>
+        <div className='line-80' />
+        <span className='get-start-free'>
+          Get Start For Free No Credit Card Required
+        </span>
+        <div className='rectangle-81'>
+          <div className='rectangle-82'>
+            <span className='services'>Services</span>
+            <div className='flex-row-bd'>
+              <div className='ellipse-83' />
+              <div className='ellipse-84' />
+              <div className='ellipse-85'>
+                <span className='score'>score</span>
+                <span className='percentage-86'>58%</span>
+              </div>
+              <div className='ellipse-87' />
+            </div>
+            <div className='flex-row-df'>
+              <span className='score-88'>score</span>
+              <span className='score-89'>score</span>
+              <span className='score-8a'>score</span>
+              <div className='rectangle-8b' />
+              <div className='rectangle-8c' />
+              <div className='rectangle-8d' />
+            </div>
+          </div>
+        </div>
+        <div className='rectangle-8e'>
+          <span className='email-address-8f'>Email address</span>
+        </div>
+        <div className='rectangle-90'>
+          <span className='get-started-91'>Get Started</span>
+        </div>
+        <div className='contact-team'>
+          <span className='contact-team-92'>
+            Want to contact our team and book a call?
+          </span>
+          <span className='try-now'>Try it now</span>
+        </div>
+      </div>
+      <div className='rectangle-93'>
+        <div className='flex-row-f-94'>
+          <div className='prosperix-95' />
+          <div className='regroup-96'>
+            <span className='our-services-97'>Our Services</span>
+            <span className='explore-more'>Explore More</span>
+            <span className='contact-details'>Contact Details</span>
+          </div>
+        </div>
+        <div className='flex-row-f-98'>
+          <span className='financial-consultants'>
+            Our expert financial consultants provide solutions to help you
+            achieve financial wealth. Trust us to guide you toward a brighter
+            financial future.
+          </span>
+          <span className='insurance-planning'>Insurance Planning</span>
+          <span className='about-us'>About us</span>
+          <span className='contact-email'>contact@briofin.com</span>
+          <span className='estate-planning-99'>Estate Planning</span>
+          <span className='blog'>Blog</span>
+          <span className='phone-number'>+1 (555) 123-4567</span>
+        </div>
+        <div className='flex-row-f-9a'>
+          <span className='tax-optimization'>Tax Optimization</span>
+          <span className='site-map'>Site map</span>
+        </div>
+        <div className='flex-row-dc'>
+          <div className='frame'>
+            <div className='instagram' />
+          </div>
+          <div className='frame-9b'>
+            <div className='facebook'>
+              <div className='vector-9c' />
+            </div>
+          </div>
+          <span className='facebook-9d'>Facebook</span>
+          <span className='twitter'>Twitter</span>
+          <span className='instagram-9e'>Instagram</span>
+          <div className='frame-9f'>
+            <div className='twitter-a0'>
+              <div className='vector-a1' />
+            </div>
+          </div>
+        </div>
+        <div className='flex-row-a-a2'>
+          <span className='debt-management'>Debt Management</span>
+          <span className='privacy'>Privacy</span>
+        </div>
+        <div className='line-a3' />
+        <div className='flex-row-e'>
+          <span className='all-rights-reserved'>
+            DSCODE 2023 © All rights reserved
+          </span>
+          <div className='regroup-a4'>
+            <span className='terms-conditions'>Terms & Conditions</span>
+            <span className='privacy-policy'>Privacy Policy</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
